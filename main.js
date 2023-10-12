b79762aed18d3519ce23ed869f86f4ba8449ab8c
Lara <larabernspereira@gmail.com>
2023-10-12 14:15:28 -0300

    layout created

---
+++ main.js
@@ -0,0 +1,8 @@
+var paragraphAnswer = document.getElementById('idParagraphAnswer')
+var textArea = document.getElementById('idTextArea')
+var buttonSubmit = document.getElementById('idButtonSubmit')
+
+function encodeDecode(event) {
+    event.preventDefault()
+    console.log('form submited')
+}
